5d3f4a7aef13d1b5d141c0e9b1477a1f6bf9a540
mridulmalpotra <mridul.malpotra@gmail.com>
2014-05-05 06:41:15 +0530

    Changed Directory structure

--- myrouter.sh
+++ myrouter.sh
@@ -9,32 +9,32 @@ do
 		n=`date "+"%s""`
 		echo "created on:" `date` >> img_config.log
 		rrdtool update myrouter.rrd $n:$in:$out
-		rrdtool graph myrouterin-1min.png --start -60 \
+		rrdtool graph ./images/myrouterin-1min.png --start -60 \
             DEF:inmaximum=myrouter.rrd:input:MAX \
             DEF:inoctets=myrouter.rrd:input:AVERAGE \
             AREA:inoctets#00FF00:"IN Average traffic" \
             LINE1:inmaximum#FF0000:"IN Maximum traffic" >> img_config.log
-        rrdtool graph myrouterout-1min.png --start -60 \
+        rrdtool graph ./images/myrouterout-1min.png --start -60 \
             DEF:outmaximum=myrouter.rrd:output:MAX \
             DEF:outoctets=myrouter.rrd:output:AVERAGE \
             AREA:outoctets#00FF00:"OUT Average traffic" \
             LINE1:outmaximum#FF0000:"OUT Maximum traffic" >> img_config.log
-		rrdtool graph myrouterin-10min.png --start -600 \
+		rrdtool graph ./images/myrouterin-10min.png --start -600 \
             DEF:inmaximum=myrouter.rrd:input:MAX \
             DEF:inoctets=myrouter.rrd:input:AVERAGE \
             AREA:inoctets#00FF00:"IN Average traffic" \
             LINE1:inmaximum#FF0000:"IN Maximum traffic" >> img_config.log
-        rrdtool graph myrouterout-10min.png --start -600 \
+        rrdtool graph ./images/myrouterout-10min.png --start -600 \
             DEF:outmaximum=myrouter.rrd:output:MAX \
             DEF:outoctets=myrouter.rrd:output:AVERAGE \
             AREA:outoctets#00FF00:"OUT Average traffic" \
             LINE1:outmaximum#FF0000:"OUT Maximum traffic" >> img_config.log
-		rrdtool graph myrouterin-1hr.png --start -3600 \
+		rrdtool graph ./images/myrouterin-1hr.png --start -3600 \
             DEF:inmaximum=myrouter.rrd:input:MAX \
             DEF:inoctets=myrouter.rrd:input:AVERAGE \
             AREA:inoctets#00FF00:"IN Average traffic" \
             LINE1:inmaximum#FF0000:"IN Maximum traffic" >> img_config.log
-        rrdtool graph myrouterout-1hr.png --start -3600 \
+        rrdtool graph ./images/myrouterout-1hr.png --start -3600 \
             DEF:outmaximum=myrouter.rrd:output:MAX \
             DEF:outoctets=myrouter.rrd:output:AVERAGE \
             AREA:outoctets#00FF00:"OUT Average traffic" \
